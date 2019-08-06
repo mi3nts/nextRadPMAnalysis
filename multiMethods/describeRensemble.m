08d634c03c01c0b53a1bfd488ef013428bc880b5
Xun Liu <xunliu@xuns-mbp.lan>
2019-08-05 23:10:46 -0700

    Add figure position helper class

--- multiMethods/describeRensemble.m
+++ multiMethods/describeRensemble.m
@@ -1,10 +1,16 @@
-function [] = describeRensemble(data, index)
+function [] = describeRensemble(data, index, figure_param)
     inputs = timetable2table(data.inputs_train, 'ConvertRowTimes', false);
     Mdl = fitrensemble(inputs, data.targets_train.(index), 'CrossVal', 'on');
     
     kflc = kfoldLoss(Mdl,'Mode','cumulative');
-    figure;
+    
+    % Plot EMS approaching stable during training epoches.
+    figure('Name', 'FitREnsemble', 'Position', figure_param.loc);
+    figure_param.refresh;
     plot(kflc);
+    title('\fontsize{15} FitREnsemble Training EMS-LearningEpoches Plot', 'Interpreter', 'tex');
     ylabel('10-fold cross-validated MSE');
     xlabel('Learning cycle');
+    
+    % Calculate result RMES and R value.
 end
--- multiMethods/main.m
+++ multiMethods/main.m
@@ -4,6 +4,9 @@ clear; close all; clc;
 % Set up the target variable index. (37 target variables, 1 -> pm10_grimm, etc.)
 wanted_target_index = 1;
 
+% Set up figures alignment.
+figure_param = FigureParam(get(groot, 'ScreenSize'), 5, 5, 10, 10);
+
 %--------------------------------------------------------------------------
 % load data.
 [data] = loadTable('mints_NextRadWithGrimmAndFWNode_1_1_data_from_12-Feb-2019_to_11-Apr-2019', 0.05);
